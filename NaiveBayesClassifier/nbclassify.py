e104dbcbcf2c73dff91b78f4caf798a46bf32c79
anchalkpr <anchalkpr188@gmail.com>
2017-02-14 14:06:21 -0800

    Naive Bayes Classifier

---
+++ NaiveBayesClassifier/nbclassify.py
@@ -0,0 +1,78 @@
+from collections import Counter
+import sys
+
+class NaiveBayesClassifier:
+    def __init__(self, file1):
+        self.tokens = {}
+        self.total_words = 0
+        self.modelParametersFile = "nbmodel.txt"
+        self.dataFile = file1
+        self.outputFileName = "nboutput.txt"
+        self.noOfClasses = 4
+        self.classes = ['deceptive', 'truthful', 'positive', 'negative']
+        self.priors = {}
+        
+        
+    def removePunctuation(self, line):
+        punctuations = ['.', ',', '"', ';', '/', '!', "'s", '$', '-']
+        for p in punctuations:
+            if p == '$':
+                line = line.replace(p, "$ ")
+            elif p == '-' or p== '/':
+                line = line.replace(p, " ")
+            else:
+                line = line.replace(p, "")
+        return line
+                
+    def readModelParameters(self):
+        txt_file = open(self.modelParametersFile)
+        self.total_words = int(txt_file.readline().strip())
+        for _ in range(self.noOfClasses):
+            line = txt_file.readline().strip().split(" ")
+            self.priors[line[0]] = float(line[1])
+        for _ in range(self.total_words):
+            line = txt_file.readline().strip().split(" ")
+            self.tokens[line[0]] = [float(line[1]), float(line[2]), float(line[3]), float(line[4])]
+            
+    def classify(self):
+        txt_file = open(self.dataFile, "r")
+        outputFile = open(self.outputFileName, "w")
+        for line in txt_file:
+            line = line.strip()
+            review_id = line[:20]
+            review = Counter(self.removePunctuation(line[21:].lower()).split(" "))
+            #review = Counter(line[21:].lower().strip().split(" "))
+            prob_deceptive = self.priors[self.classes[0]]
+            prob_truthful = self.priors[self.classes[1]]
+            prob_positive = self.priors[self.classes[2]]
+            prob_negative = self.priors[self.classes[3]]
+            for word, value in review.items():
+                if word in self.tokens:
+                    prob_deceptive += self.tokens[word][0] * value
+                    prob_truthful += self.tokens[word][1] * value
+                    prob_positive += self.tokens[word][2] * value
+                    prob_negative += self.tokens[word][3] * value
+            classification1 = None
+            classification2 = None
+            if prob_deceptive > prob_truthful:
+                classification1 = self.classes[0]
+            else:
+                classification1 = self.classes[1]
+            if prob_positive > prob_negative:
+                classification2 = self.classes[2]
+            else:
+                classification2 = self.classes[3]
+            outputFile.write("%s %s %s\n" %(review_id, classification1, classification2))
+            
+def nb_classifier():
+    file_name = "test-data.txt"
+    nbc = NaiveBayesClassifier(file_name)
+    nbc.readModelParameters()
+    nbc.classify()
+
+nb_classifier()
+
+"""if __name__=='__main__':
+    file_name = sys.argv[1]   
+    nb_classifier(file_name)"""      
+                    
---
+++ NaiveBayesClassifier/nblearn.py
@@ -0,0 +1,132 @@
+from collections import Counter
+import math
+import sys
+
+class NaiveBayesLearningModel:
+    
+    def __init__(self, file1, file2):
+        self.classes = ['positive', 'negative', 'deceptive', 'truthful']
+        self.train_text_file = file1
+        self.train_labels_file = file2
+        self.trainingClassification = {}
+        self.tokens = {}
+        self.priors = {}
+        self.total_tokens = 0
+        self.total_positive_tokens = 0
+        self.total_negative_tokens = 0
+        self.total_deceptive_tokens = 0
+        self.total_truthful_tokens = 0
+        self.total_words = 0
+        self.outputFileName = "nbmodel.txt"
+        
+    def removePunctuation(self, line):
+        line = line.replace("...", " ")
+        punctuations = ['.', ',', '"', ';', '/', '!', "'s", '$', '-', '?', ':', '(', ')']
+        for p in punctuations:
+            if p == '$':
+                line = line.replace(p, "$ ")
+            elif p == '-' or p == '/' or p == '.':
+                line = line.replace(p, " ")
+            else:
+                line = line.replace(p, "")
+        line = line.replace("  ", " ")
+        line = line.replace("  ", " ")
+        return line
+    
+    def readTrainLabelsFile(self):
+        with open(self.train_labels_file) as txt_file:
+            for line in txt_file:
+                line = line.strip().split(" ")
+                self.trainingClassification[line[0]] = (line[1], line[2])
+
+    def readTrainTextFile(self):
+        with open(self.train_text_file) as txt_file:
+            for line in txt_file:
+                line = line.strip()
+                review_id = line[:20]
+                review = Counter(self.removePunctuation(line[21:].lower().strip()).split(" "))
+                #review = Counter(line[21:].lower().strip().split(" "))
+                if self.trainingClassification[review_id][0] == self.classes[2]:
+                    for word in review:
+                        if word != " " and word != '':
+                            if word in self.tokens:
+                                self.tokens[word][0][0] += review[word]
+                            else:
+                                self.tokens[word] = [[review[word], 0], [0, 0], [0, 0], [0, 0]]
+                            self.total_deceptive_tokens += review[word]
+                else:
+                    for word in review:
+                        if word != " " and word != '':
+                            if word in self.tokens:
+                                self.tokens[word][1][0] += review[word]
+                            else:
+                                self.tokens[word] = [[0, 0], [review[word], 0], [0, 0], [0, 0]]
+                            self.total_truthful_tokens += review[word]
+                if self.trainingClassification[review_id][1] == self.classes[0]:
+                    for word in review:
+                        if word != " " and word != '':
+                            if word in self.tokens:
+                                self.tokens[word][2][0] += review[word]
+                            else:
+                                self.tokens[word] = [[0, 0], [0, 0], [review[word], 0], [0, 0]]
+                            self.total_positive_tokens += review[word]
+                else:
+                    for word in review:
+                        if word != " " and word != '':
+                            if word in self.tokens:
+                                self.tokens[word][3][0] += review[word]
+                            else:
+                                self.tokens[word] = [[0, 0], [0, 0], [0, 0], [review[word], 0]]
+                            self.total_negative_tokens += review[word]
+        self.poupulateTotalTokens()
+
+    
+    def poupulateTotalTokens(self):
+        self.total_tokens = self.total_deceptive_tokens + self.total_truthful_tokens + self.total_positive_tokens + self.total_negative_tokens
+        self.total_words = len(self.tokens)
+        
+    def calculatePriors(self):
+        for class_name in self.classes:
+            if class_name == "positive":
+                self.priors[class_name] = math.log(self.total_positive_tokens/float(self.total_tokens))
+            elif class_name == "negative":
+                self.priors[class_name] = math.log(self.total_negative_tokens/float(self.total_tokens))
+            elif class_name == "deceptive":
+                self.priors[class_name] = math.log(self.total_deceptive_tokens/float(self.total_tokens))
+            elif class_name == "truthful":
+                self.priors[class_name] = math.log(self.total_truthful_tokens/float(self.total_tokens))
+                
+    def calculateTokenLikelihoodProbabilities(self):
+        for word in self.tokens:
+            self.tokens[word][0][1] = math.log((self.tokens[word][0][0] + 1)/float(self.total_deceptive_tokens + self.total_words))
+            self.tokens[word][1][1] = math.log((self.tokens[word][1][0] + 1)/float(self.total_truthful_tokens + self.total_words))
+            self.tokens[word][2][1] = math.log((self.tokens[word][2][0] + 1)/float(self.total_positive_tokens + self.total_words))
+            self.tokens[word][3][1] = math.log((self.tokens[word][3][0] + 1)/float(self.total_negative_tokens + self.total_words))
+            
+    def writeToFile(self):
+        outputFile = open(self.outputFileName, "w")
+        outputFile.write("%s\n" %self.total_words)
+        for word, prob in self.priors.items():
+            line = "%s %s\n" %(word, prob)
+            outputFile.write(line)
+        for word, value in self.tokens.items():
+            line = "%s %s %s %s %s\n" %(word, value[0][1], value[1][1], value[2][1], value[3][1])
+            outputFile.write(line)
+            
+def run_nb_learn():
+    inputFile2 = "train-labels.txt"
+    inputFile1 = "train-text.txt"
+    nbl = NaiveBayesLearningModel(inputFile1, inputFile2)
+    nbl.readTrainLabelsFile()
+    nbl.readTrainTextFile()
+    nbl.calculatePriors()
+    nbl.calculateTokenLikelihoodProbabilities()
+    nbl.writeToFile()
+
+run_nb_learn()
+
+"""if __name__=='__main__':
+    file1 = sys.argv[1]
+    file2 = sys.argv[2]
+    
+    run_nb_learn(file1, file2) """ 
